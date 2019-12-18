35b6a3818bf56e8caf63d21f08a89ec31be8161d
albk <albk>
2019-12-18 23:48:30 +0800

    增加理解

---
+++ datastructure-base/src/main/java/com/albk/datastructure/algorithm/sort/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * @description:
+ * @author BK
+ * @date 2019-12-18 23:44
+ */
+package com.albk.datastructure.algorithm.sort;
+
+//冒泡排序:
+//n^2  最好是On
+//基本不用，太慢，两比较交换，比较和交换次数太多
+//
+//选择排序
+//On2 , 不稳定  基本不用， 又慢，又不稳定
+//
+//插入排序
+//On2  最好是On 稳定   样本小且基本有序的时候效率比较高
